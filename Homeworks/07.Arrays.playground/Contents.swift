79767bae2e8c95343c6de7c591c8e51dabc40b7c
brzjune <brzjune@gmail.com>
2017-10-18 13:45:31 +0300

    edited

---
+++ Homeworks/07.Arrays.playground/Contents.swift
@@ -0,0 +1,35 @@
+ //: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+//MARK: Lesson.07 - Arrays
+
+//07.task.03
+
+let alphabet07 = "abcdefghijklmnopqrstuvwxyz"
+var array07 = [String]()
+
+for i in alphabet07.characters  {
+    //array07.insert(String(i), at: 0)
+    array07.append(String(i))
+}
+print("Alphabet = \(array07)")
+print("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
